37107476be706c1f24374872cd0d9dda9545867c
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-01 12:10:49 -0500

    added new tabs and updated the README

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -31,7 +31,6 @@ class TabLink {
   }
 
   selectTab(){
-
     // Select all elements with the .tab class on them
     // const tabs = document.querySelectorAll();
     const tabs = document.querySelectorAll(".tab");
@@ -69,6 +68,72 @@ class TabCard {
     this.cardElement.style.display = "flex";
   }
 }
+
+const articlesData = {
+  articleIndexes: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  articleTitles: [
+    "So, remember the new number! 0118 999! 88199, 9119 725! ... 3!",
+    "I'm a 58 year old IT-man who works in a basement. Yes, I do the whole Lambda Times thing!",
+    "We don't need no education. Yes you do. You've just used a double negative.",
+    "I am a man, he's a man, we're men! Ok, tell me how your feeling.",
+    "Someone stole his keyboard. Did you see who it was? Red bearded man.",
+    "How long have you been programming? Ten years? Ten years, and how did it happen? If that's not a rude question.",
+    "Yeah, you need to turn it on... uh, the button turns it on.",
+    "See the driver hooks a function by patching the system call table, so its not safe to unload it.",
+    "It's not safe to unload it unless another thread's about to jump in there and do its stuff, and you don't want to end up in the middle of invalid memory!",
+    "Hello, IT. Have you tried turning it off and on again?",
+    "I am writing to inform you of a fire which has broken out at the premises of...",
+    "Warning: Contains scenes of programming.",
+    "Shut up, do what I tell you, I'm not interested"
+  ],
+  tabs: ["javascript", "technology", "node", "jquery", "bootstrap"],
+  authors: [
+    {name: "Sir Ruff'n'Stuff", img: "sir.jpg"},
+    {name: "Bones R. Life", img: "bones.jpg"},
+    {name: "Fido Walksalot", img: "fido.jpg"},
+    {name: "Max Goodboye", img: "max.jpg"},
+    {name: "Pupper S. Doggo", img: "puppers.jpg"},
+  ] 
+};
+
+function generateArticles(){
+  articlesData.articleIndexes.forEach((i) => {
+    let tab = articlesData.tabs[Math.floor(Math.random()*articlesData.tabs.length)];
+    let authorData = articlesData.authors[Math.floor(Math.random()*articlesData.authors.length)];
+    let articleTitle = articlesData.articleTitles[Math.floor(Math.random()*articlesData.articleTitles.length)];
+
+    let card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('data-tab', tab);
+
+    let headline = document.createElement('div');
+    headline.classList.add('headline');
+    headline.textContent = `${articleTitle}`;
+    card.appendChild(headline);
+
+    let author = document.createElement('div');
+    author.classList.add('author');
+
+    let img_container = document.createElement('div');
+    img_container.classList.add('img-container');
+
+    let img = document.createElement('img');
+    img.setAttribute('src', `./assets/${authorData.img}`);
+    
+    img_container.appendChild(img);
+    author.appendChild(img_container);
+
+    let span = document.createElement('span');
+    span.textContent = `By ${authorData.name.toUpperCase()}`;
+
+    author.appendChild(span);
+
+    card.appendChild(author);
+    document.querySelector('.cards-container').appendChild(card);
+  });
+}
+
+generateArticles();
 
 /* START HERE: 
 
