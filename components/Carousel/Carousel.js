3242aeab39fccf75197a6fc0d3050a2c18f9cdd3
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-01 14:00:50 -0500

    added a carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,72 @@
 class Carousel {
+    constructor(element){
+        this.element = element;
+
+        this.slides = [];
+        this.navs = [];
+
+        this.currentlySelected = 0;
+
+        this.renderSlides();
+        this.renderSlideNavs();
+
+        this.playCarousel();
+    }
+
+    renderSlides(){
+        this.element.querySelectorAll('img').forEach(slide => {
+            slide.classList.add('default');
+            this.slides.push(new Slide(slide));
+        });
+    }
+
+    renderSlideNavs(){
+        this.element.querySelectorAll('div').forEach(nav => this.navs.push(new SlideNav(nav)));
+    }
+
+    updateCurrentlySelected(direction){
+        this.slides[this.currentlySelected % this.slides.length].toggle();
+        this.currentlySelected += direction ? 1 : -1;
+        this.slides[this.currentlySelected % this.slides.length].toggle();
+    }
+
+    playCarousel(){
+        this.slides[this.currentlySelected % this.slides.length].toggle();
+    }
+}
+
+class SlideNav {
+    constructor(element){
+        this.element = element;
+
+        this.element.addEventListener('click', () => this.changeSlide());
+    }
+
+    changeSlide(){
+        return ref.updateCurrentlySelected(this.element.classList.contains('right-button') ? true : false);
+    }
 
 }
 
-let carousel = document.querySelector();
+class Slide {
+    constructor(element){
+        this.element = element;
+    }
+
+    toggle(){
+        this.element.classList.toggle('show');
+
+        if(this.element.classList.contains('show')){
+            TweenMax.to(this.element, .5, {left: 0});
+        }else{
+            TweenMax.to(this.element, .5, {left: -1000});
+        };
+
+    }
+}
+
+let carousel = document.querySelector('.carousel');
+let ref = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
